f432300cbb8905086aa8aa3f1e7c1466b5d34089
natee <kerncink@gmail.com>
2017-10-17 11:51:40 +0800

    update

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # javascript-number
 javascript中对number的处理
 
-###javascript中number加减乘除精度处理
+### 现象 ###
+0.1 + 0.2 = 0.30000000000000004
+1.0 - 0.9 = 0.09999999999999998
 
-###javascript数值类型判断（整数、小数、自然数...）
+### javascript中number加减乘除精度处理 ###
+#### 方法
+加法：(0.1).add(0.2)
+减法：(0.1).sub(0.2)
+乘法：(0.1).mul(0.2)
+除法：(0.1).div(0.2)
+
+### javascript数值类型判断（整数、小数、自然数...）###
+
+
+### 参考
+之前的在某些条件仍然计算错误，参照number-precision进行了修改。
+[number-precision](https://github.com/dt-fe/number-precision)
--- number.js
+++ number.js
@@ -3,74 +3,82 @@
  * 如：0.9-0.8=0.09999999999998
  * 调用方法 (number1).fn(number2)
  */
-(function () {
-    function add(first, second) {
-        var l1 = 0;
-        var l2 = 0;
-        try{
-            l1 = first.toString().split(".")[1].length;
-        }catch(e){
-
-        }
-        try{
-            l2 = second.toString().split(".")[1].length;
-        }catch(e){
-
-        }
-        var m = Math.pow(10, Math.max(l1, l2));
-        return (first * m + second * m) / m;
+(function() {
+    /**
+     * 把错误的数据转正
+     * strip(0.09999999999999998)=0.1
+     */
+    function strip(num, precision = 12) {
+        return +parseFloat(num.toPrecision(precision));
     }
 
-//加法
-    Number.prototype.add = function (number) {
-        return add.call(this, this, number);
-    };
-
-//减法，等同于加法
-    Number.prototype.sub = function (number) {
-        return this.add(-number);
-    };
-
-    function mul(first, second) {
-        var l1 = 0;
-        var l2 = 0;
-        try{
-            l1 = first.toString().split(".")[1].length;
-        }catch(e){
-
-        }
-        try{
-            l2 = second.toString().split(".")[1].length;
-        }catch(e){
-
-        }
-        return ((first*Math.pow(10,l1)) * (second*Math.pow(10,l2))) / Math.pow(10, l1+l2);
+    /**
+     * Return digits length of a number
+     * @param {*number} num Input number
+     */
+    function digitLength(num) {
+        // Get digit length of e
+        var eSplit = num.toString().split(/[eE]/);
+        var len = (eSplit[0].split('.')[1] || '').length - (+(eSplit[1] || 0));
+        return len > 0 ? len : 0;
     }
 
-//乘法
-    Number.prototype.mul = function (number) {
-        return mul.call(this, this, number);
-    };
-
-    function div(first, second) {
-        var l1 = 0;
-        var l2 = 0;
-        try{
-            l1 = first.toString().split(".")[1].length;
-        }catch(e){
-
-        }
-        try{
-            l2 = second.toString().split(".")[1].length;
-        }catch(e){
-
-        }
-        var m = Math.pow(10, Math.max(l1, l2));
-        return (first*m) / (second*m);
+    /**
+     * 精确乘法
+     */
+    function times(num1, num2) {
+        var num1Changed = Number(num1.toString().replace('.', ''));
+        var num2Changed = Number(num2.toString().replace('.', ''));
+        var baseNum = digitLength(num1) + digitLength(num2);
+        return num1Changed * num2Changed / Math.pow(10, baseNum);
     }
 
-//除法
-    Number.prototype.div = function (number) {
-        return div.call(this, this, number)
+
+    /**
+     * 精确加法
+     */
+    function plus(num1, num2) {
+        var baseNum = Math.pow(10, Math.max(digitLength(num1), digitLength(num2)));
+        return (times(num1, baseNum) + times(num2, baseNum)) / baseNum;
+    }
+
+    /**
+     * 精确减法
+     */
+    function minus(num1, num2) {
+        var baseNum = Math.pow(10, Math.max(digitLength(num1), digitLength(num2)));
+        return (num1 * baseNum - num2 * baseNum) / baseNum;
+    }
+
+    /**
+     * 精确除法
+     */
+    function divide(num1, num2) {
+        var num1Changed = Number(num1.toString().replace('.', ''));
+        var num2Changed = Number(num2.toString().replace('.', ''));
+        return times((num1Changed / num2Changed), Math.pow(10, digitLength(num2) - digitLength(num1)));
+    }
+
+
+    //加法
+    Number.prototype.add = function(number){
+        return plus.call(this, this, number);
+    };
+
+    //减法，等同于加法
+    Number.prototype.sub = function(number){
+        return minus.call(this, this, number);
+    };
+
+
+    //乘法
+    Number.prototype.mul = function(number){
+        return times.call(this, this, number);
+    };
+
+
+    //除法
+    Number.prototype.div = function(number){
+        return divide.call(this, this, number);
     };
 })();
